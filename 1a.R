9948db909383c999604fdcd4f9394dcc08c52ca6
pooja r <56813166+poojarhyr@users.noreply.github.com>
2019-11-07 13:12:38 +0530

    Add files via upload

---
+++ 1a.R
@@ -0,0 +1,8 @@
+path="C:/Users/jyothiramesh/Desktop/DSRLAB/DATA_SET"
+setwd(path)
+dataval=read.csv("iris.csv")
+dataval
+#sepal.length
+#sepal.width
+
+plot(dataval$sepal.length,dataval$sepal.width)
